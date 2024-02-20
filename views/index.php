ec3a09d5e64a57c1cb3c11295164fa4721ddf7d2
JoniSchimansky <jonischimansky@gmail.com>
2024-02-20 12:31:10 +0100

    Structure setup

---
+++ views/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PVP price switcher
+ *
+ * @author    Reborn Media Studio <info@rebornmediastudio.com>
+ * @copyright Reborn Media Studio 2024
+ * @license   Commercial
+ * @version   3.2.1
+ *
+ */
+
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+header("Location: ../");
+exit;
