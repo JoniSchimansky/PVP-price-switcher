a5fc9ae4f5a4641b0fc15a56ae5f962e7451c393
JoniSchimansky <jonischimansky@gmail.com>
2024-02-21 11:58:39 +0100

    testing

---
+++ views/js/front.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Aquí iría la lógica relacionada con el módulo una vez que el DOM esté completamente cargado.
+    console.log('El archivo front.js del módulo pvppriceswitcher se ha cargado correctamente.');
+});
