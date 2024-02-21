1dd387cf929f361b81712d19cf7f6b59118b6fd1
JoniSchimansky <jonischimansky@gmail.com>
2024-02-21 11:52:49 +0100

    translation

---
+++ translations/es.php
@@ -0,0 +1,20 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_9a730ebf1f0f9df30deb7e11cd660dd6'] = 'Cambio de impuestos de precio';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_74a0997beb048579bd27326f526c23cd'] = 'Muestre el precio del producto en su tienda con impuestos incluidos o excluidos con un solo clic.';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_bb8956c67b82c7444a80c6b2433dd8b4'] = '¿Está seguro de que desea desinstalar este módulo?';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_bf50d5e661106d0abe925af3c2e6f7e7'] = 'Encabezamiento';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_6483df61619946ebf2427ba360df7ce5'] = 'La configuración se ha actualizado correctamente.';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_f4f70727dc34561dfde1a3c529b6205c'] = 'Configuraciones';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_52f5e0bc3859bc5f5e25130b6c7e8881'] = 'Posición';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_21489e68c817a96cb1421b2de2fa82ae'] = 'Seleccione la posición específica donde desea mostrar el widget de cambio de impuestos.';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_ae284f900f9d6e21ba69144cfc91e41b'] = 'Estilo';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_2116fa7dc9eed9339cb316b2499a2b81'] = 'Desplegable';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_1f32d2c1b49b9b0c9d99bd10da107e41'] = 'Radio';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_0271e86b4d1252b77d0e3de30244d21f'] = 'Configure el estilo del widget del conmutador de impuestos para que se muestre en la oficina principal.';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_c9cc8cce247e49bae79f15173ce97354'] = 'Salvar';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>nxtalpricetaxswitcher_c1cdc36000985e7b9dc222c457acb4ba'] = 'Cambiador de impuestos';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>element_befcac0f9644a7abee43e69f49252ac4'] = 'Sin impuestos';
+$_MODULE['<{nxtalpricetaxswitcher}prestashop>element_f4a0d7cb0cd45214c8ca5912c970de13'] = 'Impuesto incluido';
