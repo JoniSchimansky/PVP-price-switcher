0104fe830ca9c5667015be5ed1e6f11c222a48e3
JoniSchimansky <jonischimansky@gmail.com>
2024-02-21 12:21:22 +0100

    test

--- pvppriceswitcher.php
+++ pvppriceswitcher.php
@@ -18,11 +18,11 @@ class PvpPriceSwitcher extends Module
     public function __construct()
     {
         $this->name = 'pvppriceswitcher';
-        $this->tab = 'pricing_promotion';
+        $this->tab = 'front_office_features';
         $this->version = '1.0.0';
         $this->author = 'Reborn Media Studio';
         $this->need_instance = 0;
-        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
         $this->bootstrap = true;
 
         parent::__construct();
@@ -31,44 +31,168 @@ class PvpPriceSwitcher extends Module
         $this->description = $this->l('Allows B2B customers to toggle between tax-excluded and tax-included prices.');
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        $this->hooks = array(
+            array(
+                'id' => 'displayNav',
+                'name' => $this->l('Header'),
+            )
+        );
+
+        $this->configVars = array(
+            'NXTAL_PRICE_TAX_SWITCHER_HOOK',
+            'NXTAL_PRICE_TAX_SWITCHER_STYLE',
+            'NXTAL_PRICE_TAX_PRODUCT_PAGE',
+        );
+
+        $this->configValues = $this->getConfigValue();
     }
 
     public function install()
     {
-        if (Shop::isFeatureActive()) {
-            Shop::setContext(Shop::CONTEXT_ALL);
-        }
-        
         return parent::install() &&
-               $this->registerHook('displayNav') &&
-               $this->registerHook('header') &&
-               $this->registerHook('backOfficeHeader');
+            $this->registerHook('displayNav') &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader') &&
+            Configuration::updateValue('NXTAL_PRICE_TAX_SWITCHER_HOOK', 'displayNav');
     }
 
     public function uninstall()
     {
+        foreach ($this->configVars as $key) {
+            Configuration::deleteByName($key);
+        }
+
         return parent::uninstall();
     }
 
+    public function getConfigValue()
+    {
+        $values = array();
+        foreach ($this->configVars as $key) {
+            $values[$key] = Tools::getValue($key, Configuration::get($key));
+        }
+        return $values;
+    }
+
+    /**
+     * Load the configuration form
+     */
     public function getContent()
     {
-        $output = null;
-
-        if (Tools::isSubmit('submit'.$this->name)) {
-            // Procesar la información del formulario de configuración
-            $output .= $this->displayConfirmation($this->l('Settings updated'));
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool)Tools::isSubmit('submit'.$this->name)) == true) {
+            $this->postProcess();
+            $this->context->controller->confirmations[] = $this->l('The settings have been successfully updated.');
         }
-        return $output.$this->displayForm();
+
+        return $this->renderForm() . $this->displayPromo();
     }
 
-    protected function postProcess()
-    {
-        // Procesar y guardar la configuración enviada por el usuario
-    }
-
+     /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
     protected function renderForm()
     {
-        // Construir y devolver el formulario de configuración
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submit'.$this->name;
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->configValues, /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'select',
+                        'label' => $this->l('Style'),
+                        'name' => 'NXTAL_PRICE_TAX_SWITCHER_STYLE',
+                        'options' => array(
+                            'query' => array(
+                                array(
+                                    'id' => 0,
+                                    'name' => $this->l('Switch')
+                                ),
+                                array(
+                                    'id' => 1,
+                                    'name' => $this->l('Switch Interactive')
+                                ),
+                                array(
+                                    'id' => 2,
+                                    'name' => $this->l('Drop Down')
+                                ),
+                                array(
+                                    'id' => 3,
+                                    'name' => $this->l('Radio')
+                                )
+                            ),
+                            'id' => 'id',
+                            'name' => 'name'
+                        ),
+                        'desc' => $this->l('Set tax switcher widget style to display in front office.')
+                    )
+                    array(
+                        'type' => 'switch',
+                        'name' => 'NXTAL_PRICE_TAX_PRODUCT_PAGE',
+                        'label' => $this->l('Display switch at product page'),
+                        'desc' => $this->l('Enable to display tax switch at product detail page.'),
+                        'is_bool' => true,
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => 1,
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => 0,
+                            ),
+                        )
+                    ),
+
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        foreach ($this->configVars as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
     }
 
     public function hookDisplayNav($params)
